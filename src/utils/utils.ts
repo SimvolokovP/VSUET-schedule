adab49b05b6240fd2730239a4339a0047f7a22d1
Simvolokov Pavel <psimvolokov@gmail.com>
2024-10-07 20:30:00 +0300

    fix weekType error

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,16 @@
 export const getCurrentDayAndWeekType = () => {
-  const startDate = new Date("2024-08-01");
   const currentDate = new Date();
-  const diffTime = Math.abs(currentDate.getTime() - startDate.getTime());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  const weekNumber = Math.floor(((diffDays + startDate.getDay() + 6) % 7) / 7);
+
+  const weekNumber = Math.ceil(
+    (currentDate.getTime() -
+      new Date(currentDate.getFullYear(), 0, 1).getTime()) /
+      1000 /
+      60 /
+      60 /
+      24 /
+      7
+  );
+
   const weekType = weekNumber % 2 === 0 ? "Числитель" : "Знаменатель";
 
   const currentDay = currentDate.toLocaleString("ru-RU", { weekday: "long" });
